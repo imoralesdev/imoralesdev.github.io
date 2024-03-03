b8052b9e6eebfceb19d14408c979db26bfe98525
Israel Morales <77509494+imoralesdev@users.noreply.github.com>
2024-03-03 17:12:10 -0500

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,29 +1,27 @@
 console.log("JavaScript Load");
 
-let canvas = document.querySelector('#hero_canvas');
-let ctx = canvas.getContext('2d');
+var canvas = document.querySelector('#hero_canvas'); // Added '#' for ID selector
+var ctx = canvas.getContext('2d');
 
-class Pixel {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.hue = Math.floor(Math.random() * 360);
-    const direction = Math.random() > 0.5 ? -1 : 1;
-    this.velocity = (Math.random() * 30 + 20) * 0.01 * direction;
-  }
-
-  update() {
-    this.hue += this.velocity;
-  }
-
-  render(ctx) {
-    const hue = Math.round(this.hue);
-    ctx.fillStyle = `hsl(${hue}, 100%, 50%)`; // Template literals for cleaner string
-    ctx.fillRect(this.x, this.y, 1, 1);
-  }
+function Pixel(x, y) {
+  this.x = x;
+  this.y = y;
+  this.hue = Math.floor(Math.random() * 360);
+  var direction = Math.random() > 0.5 ? -1 : 1;
+  this.velocity = (Math.random() * 30 + 20) * 0.01 * direction;
 }
 
-const pixels = [
+Pixel.prototype.update = function() {
+  this.hue += this.velocity;
+};
+
+Pixel.prototype.render = function(ctx) {
+  var hue = Math.round(this.hue);
+  ctx.fillStyle = 'hsl(' + hue + ', 100%, 50%)'; // Keep concatenation for older browsers
+  ctx.fillRect(this.x, this.y, 1, 1);
+};
+
+var pixels = [
   new Pixel(0, 0),
   new Pixel(1, 0),
   new Pixel(0, 1),
@@ -31,7 +29,7 @@ const pixels = [
 ];
 
 function animate() {
-  pixels.forEach(pixel => { // Arrow function for cleaner syntax
+  pixels.forEach(function(pixel) { // Keep function keyword for older browsers
     pixel.update();
     pixel.render(ctx);
   });
