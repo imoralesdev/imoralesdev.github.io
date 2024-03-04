5540bfdcca1f1f3d153c8b06839ec45b750bdb60
Israel Morales <77509494+imoralesdev@users.noreply.github.com>
2024-03-03 19:49:24 -0500

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,50 +1,51 @@
 console.log("JavaScript Load");
 
-var container = document.getElementById('hero_canvas_container');
-var canvas = document.createElement('canvas');
+var canvas = document.querySelector('canvas');
 var ctx = canvas.getContext('2d');
 
-container.appendChild(canvas);
+// Set canvas to full width and height of the window
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-function setCanvasSize() {
-  canvas.width = container.offsetWidth;
-  canvas.height = container.offsetHeight;
-}
-
-// Initial set up of canvas size
-setCanvasSize();
-
-function Pixel( x, y ) {
+function Pixel(x, y) {
   this.x = x;
   this.y = y;
-  this.hue = Math.floor( Math.random() * 360 );
+  this.hue = Math.floor(Math.random() * 360);
   var direction = Math.random() > 0.5 ? -1 : 1;
-  this.velocity = ( Math.random() * 30 + 20 ) * 0.01 * direction;
+  this.velocity = (Math.random() * 30 + 20) * 0.01 * direction;
 }
 
 Pixel.prototype.update = function() {
   this.hue += this.velocity;
+  // Reset the hue to keep it within the 0-360 range
+  if (this.hue > 360) this.hue -= 360;
+  if (this.hue < 0) this.hue += 360;
 };
 
-Pixel.prototype.render = function( ctx ) {
-  var hue = Math.round( this.hue );
-  ctx.fillStyle = 'hsl(' + hue + ', 100%, 50% )';
-  ctx.fillRect( this.x, this.y, 1, 1 );
+Pixel.prototype.render = function(ctx) {
+  var hue = Math.round(this.hue);
+  ctx.fillStyle = 'hsl(' + hue + ', 100%, 50%)';
+  ctx.fillRect(this.x, this.y, 1, 1); // Size of each pixel is 1x1
+};
+
+// Generate pixels for the entire canvas
+var pixels = [];
+for (var y = 0; y < canvas.height; y++) {
+  for (var x = 0; x < canvas.width; x++) {
+    pixels.push(new Pixel(x, y));
+  }
 }
 
-var pixels = [
-  new Pixel( 0, 0 ),
-  new Pixel( 1, 0 ),
-  new Pixel( 0, 1 ),
-  new Pixel( 1, 1 ),
-];
-
 function animate() {
-  pixels.forEach( function( pixel ) {
+  // Clear the canvas before each frame
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  pixels.forEach(function(pixel) {
     pixel.update();
-    pixel.render( ctx );
+    pixel.render(ctx);
   });
-  requestAnimationFrame( animate );
+
+  requestAnimationFrame(animate);
 }
 
 animate();
