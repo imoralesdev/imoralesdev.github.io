0ebd1113116685a669279d5935dc1407a905a722
Israel Morales <77509494+imoralesdev@users.noreply.github.com>
2024-03-03 20:13:10 -0500

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,45 +2,7 @@ console.log("JavaScript Load");
 
 var canvas = document.getElementById('hero_canvas_container');
 var ctx = canvas.getContext('2d');
-/*
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
 
-function Pixel( x, y ) {
-  this.x = x;
-  this.y = y;
-  this.hue = Math.floor( Math.random() * 360 );
-  var direction = Math.random() > 0.5 ? -1 : 1;
-  this.velocity = ( Math.random() * 30 + 20 ) * 0.01 * direction;
-}
-
-Pixel.prototype.update = function() {
-  this.hue += this.velocity;
-};
-
-Pixel.prototype.render = function( ctx ) {
-  var hue = Math.round( this.hue );
-  ctx.fillStyle = 'hsl(' + hue + ', 100%, 50% )';
-  ctx.fillRect( this.x, this.y, 1, 1 );
-}
-
-var pixels = [
-  new Pixel( 0, 0 ),
-  new Pixel( 1, 0 ),
-  new Pixel( 0, 1 ),
-  new Pixel( 1, 1 ),
-];
-
-function animate() {
-  pixels.forEach( function( pixel ) {
-    pixel.update();
-    pixel.render( ctx );
-  });
-  requestAnimationFrame( animate );
-}
-
-animate();
-*/
 // Set canvas to full width and height of the window
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
