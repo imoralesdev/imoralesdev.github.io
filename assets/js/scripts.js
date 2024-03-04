664c9cb9d9064328407c50250ed8e23e06908bcc
Israel Morales <77509494+imoralesdev@users.noreply.github.com>
2024-03-03 20:15:56 -0500

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,5 @@
 console.log("JavaScript Load");
-
+/*
 var canvas = document.getElementById('hero_canvas_container');
 var ctx = canvas.getContext('2d');
 
@@ -23,5 +23,42 @@ function animate() {
 
   requestAnimationFrame(animate);
 }
+
+animate();
+*/
+var canvas = document.getElementById('hero_canvas_container');
+var ctx = canvas.getContext('2d');
+
+// Function to set canvas size
+function setCanvasSize() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+
+// Initial canvas size set
+setCanvasSize();
+
+var hue = 0;
+var velocity = 0.1; // Speed of hue change
+
+function animate() {
+  hue += velocity;
+  hue = hue % 360; // Keep the hue within the 0-360 range
+
+  var gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
+  gradient.addColorStop(0, 'hsl(' + hue + ', 100%, 50%)');
+  gradient.addColorStop(1, 'hsl(' + ((hue + 180) % 360) + ', 100%, 50%)');
+
+  ctx.fillStyle = gradient;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  requestAnimationFrame(animate);
+}
+
+// Event listener for window resize
+window.addEventListener('resize', function() {
+  setCanvasSize();
+  // You might want to call animate() here if the animation stops on resize
+});
 
 animate();
