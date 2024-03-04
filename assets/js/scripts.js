4d60b1a3d7e6bb5a0ad9217a8031fa477473c525
Israel Morales <77509494+imoralesdev@users.noreply.github.com>
2024-03-03 19:28:47 -0500

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -14,54 +14,37 @@ function setCanvasSize() {
 // Initial set up of canvas size
 setCanvasSize();
 
-function Pixel() {
-  this.x = Math.random() * canvas.width;
-  this.y = Math.random() * canvas.height;
-  this.hue = Math.floor(Math.random() * 360);
+function Pixel( x, y ) {
+  this.x = x;
+  this.y = y;
+  this.hue = Math.floor( Math.random() * 360 );
   var direction = Math.random() > 0.5 ? -1 : 1;
-  this.velocity = (Math.random() * 30 + 20) * 0.01 * direction;
+  this.velocity = ( Math.random() * 30 + 20 ) * 0.01 * direction;
 }
 
 Pixel.prototype.update = function() {
   this.hue += this.velocity;
-  // Ensure the hue stays within the 0-360 range
-  if (this.hue >= 360 || this.hue <= 0) {
-    this.velocity *= -1;
-  }
 };
 
-Pixel.prototype.render = function() {
-  var hue = Math.round(this.hue);
-  ctx.fillStyle = 'hsl(' + hue + ', 100%, 50%)';
-  ctx.fillRect(this.x, this.y, 1, 1);
-};
-
-// Create a dynamic number of pixels based on the canvas size
-var pixels = [];
-function createPixels() {
-  pixels = [];
-  var pixelDensity = 10000; // Adjust pixel density as needed
-  var pixelCount = (canvas.width * canvas.height) / pixelDensity;
-  for (var i = 0; i < pixelCount; i++) {
-    pixels.push(new Pixel());
-  }
+Pixel.prototype.render = function( ctx ) {
+  var hue = Math.round( this.hue );
+  ctx.fillStyle = 'hsl(' + hue + ', 100%, 50% )';
+  ctx.fillRect( this.x, this.y, 1, 1 );
 }
 
-createPixels(); // Initial creation of pixels
+var pixels = [
+  new Pixel( 0, 0 ),
+  new Pixel( 1, 0 ),
+  new Pixel( 0, 1 ),
+  new Pixel( 1, 1 ),
+];
 
 function animate() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
-  pixels.forEach(function(pixel) {
+  pixels.forEach( function( pixel ) {
     pixel.update();
-    pixel.render();
+    pixel.render( ctx );
   });
-  requestAnimationFrame(animate);
+  requestAnimationFrame( animate );
 }
 
 animate();
-
-// Handle window resize
-window.addEventListener('resize', function() {
-  setCanvasSize();
-  createPixels(); // Recreate pixels for the new size
-});
