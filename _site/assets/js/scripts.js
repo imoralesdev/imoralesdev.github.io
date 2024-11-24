854d94edc250c5ed5339bf078c15f28d72e5b19b
imoralesdev <imorales.dev@gmail.com>
2024-11-24 12:46:29 -0500

    bug on homepage

--- _site/assets/js/scripts.js
+++ _site/assets/js/scripts.js
@@ -1,7 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const emailUser = "imoralescs";
-    const emailDomain = "gmail.com";
-    const emailElement = document.getElementById("email-link");
-    const emailAddress = `${emailUser}@${emailDomain}`;
-    emailElement.innerHTML = `<a href="mailto:${emailAddress}">Contact</a>`;
-});
+console.log("JavaScript Load");
