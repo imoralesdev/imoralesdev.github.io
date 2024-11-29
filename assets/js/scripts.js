330e8c9246d92804363c3c713979ae7e25d93d94
imoralesdev <imorales.dev@gmail.com>
2024-11-29 00:44:46 -0500

    fix image presentation

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,45 @@
-console.log("JavaScript Load");
+const username = 'nixcko';
+const blogSection = document.getElementById("blog-post");
+
+const articles = fetch(`https://dev.to/api/articles?username=${username}`)
+.then((res) => res.json())
+.then((data) => {
+  // Call a function to populate the blog cards with fetched data
+  populateBlogCards(data);
+  console.log(data)
+})
+.catch((err) => console.error("Error fetching articles:", err));
+
+function populateBlogCards(articles) {
+    let blogCardContainer = blogSection.querySelector(".blog-post__list");
+  
+    // If the container doesn't exist, create it
+    if (!blogCardContainer) {
+      blogCardContainer = document.createElement("ul");
+      blogCardContainer.classList.add("blog-post__list");
+      blogSection.appendChild(blogCardContainer);
+    }
+  
+    // Clear existing content
+    blogCardContainer.innerHTML = "";
+  
+    // Loop through each article and create blog card elements
+    articles.forEach((article) => {
+      const listItem = document.createElement("li");
+      listItem.classList.add("blog-post__item");
+  
+      const cardLink = document.createElement("a");
+      cardLink.href = article.url;
+      cardLink.target = "_blank";
+      cardLink.classList.add("blog-post__card");
+      cardLink.innerHTML = `
+        <h3 class="blog-post__name">${article.title}</h3>
+        <p class="blog-post__description">
+          ${article.description ? article.description : "No description available"}
+        </p>
+      `;
+  
+      listItem.appendChild(cardLink);
+      blogCardContainer.appendChild(listItem);
+    });
+  }
